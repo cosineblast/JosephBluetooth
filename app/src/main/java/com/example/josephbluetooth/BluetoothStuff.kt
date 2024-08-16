7bcf416bb9fa0949916dfcb39f141016ee3456a9
cosineblast <55855728+cosineblast@users.noreply.github.com>
2024-08-16 00:26:20 -0300

    Refactor bluetooth message code

--- app/src/main/java/com/example/josephbluetooth/BluetoothStuff.kt
+++ app/src/main/java/com/example/josephbluetooth/BluetoothStuff.kt
@@ -1,1 +1,84 @@
 package com.example.josephbluetooth
+
+
+import android.annotation.SuppressLint
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
+import android.bluetooth.BluetoothSocket
+import android.util.Log
+import com.example.josephbluetooth.MainActivity.Message
+import org.json.JSONObject
+import java.io.DataOutputStream
+import java.io.OutputStream
+import kotlin.concurrent.thread
+
+class BluetoothStuff : AutoCloseable {
+
+    var bluetoothSocket: BluetoothSocket? = null
+
+    fun serializeMessage(message: Message, stream: OutputStream) {
+        val result = JSONObject()
+        result.put("x", message.x)
+        result.put("y", message.y)
+        result.put("z", message.z)
+
+        val bytes = result.toString().toByteArray(Charsets.UTF_8)
+
+        val newStream = DataOutputStream(stream)
+
+        newStream.writeInt(bytes.size)
+        newStream.write(bytes)
+        newStream.flush()
+    }
+
+    fun sendMessage(message: Message) {
+        val socket = bluetoothSocket!!
+
+        serializeMessage(message, socket.outputStream)
+    }
+
+
+    @SuppressLint("MissingPermission")
+    fun connectBluetooth(adapter: BluetoothAdapter, onConnect: (BluetoothSocket) -> Unit) {
+
+        val pairedDevices: Set<BluetoothDevice>? = adapter.bondedDevices
+
+        if (pairedDevices == null) {
+            Log.d("seila", "Não consegui arranjar dispositivos :(")
+            return
+        }
+
+        Log.d("seila", "Arranjei os ${pairedDevices.size} dispositivos")
+
+        // TODO: mostrar em interface gráfica lista de dispositivos para usuário selecionar e parear
+        // TODO: ou colocar o MAC address do seu PC
+        val device = pairedDevices.find { it.address == "88:78:73:13:3A:C0" }
+
+        if (device == null) {
+            Log.d("seila", "Não encontrei o pc >-<")
+            return
+        }
+
+        thread {
+            adapter.cancelDiscovery()
+
+            // que horror
+            val socket = device.javaClass.getMethod("createRfcommSocket", *arrayOf(Int::class.java))
+                .invoke(device, 1) as BluetoothSocket
+
+            Log.d("seila", "consegui o socket")
+
+            socket.connect()
+
+            Log.d("seila", "conectado no socket")
+
+            bluetoothSocket = socket
+
+            onConnect(socket)
+        }
+    }
+
+    override fun close() {
+        bluetoothSocket?.close()
+    }
+}
--- app/src/main/java/com/example/josephbluetooth/MainActivity.kt
+++ app/src/main/java/com/example/josephbluetooth/MainActivity.kt
@@ -1,64 +1,14 @@
 package com.example.josephbluetooth
 
-import android.annotation.SuppressLint
-import android.bluetooth.BluetoothAdapter
-import android.bluetooth.BluetoothDevice
-import android.bluetooth.BluetoothSocket
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import org.json.JSONObject
-import java.io.DataOutputStream
-import java.io.OutputStream
-import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
 
-    var bluetoothSocket: BluetoothSocket? = null
-
-    @SuppressLint("MissingPermission")
-    fun connectBluetooth(adapter: BluetoothAdapter) {
-
-        val pairedDevices: Set<BluetoothDevice>? = adapter.bondedDevices
-
-        if (pairedDevices == null) {
-            Log.d("seila", "Não consegui arranjar dispositivos :(")
-            return
-        }
-
-        Log.d("seila", "Arranjei os ${pairedDevices.size} dispositivos")
-
-        // TODO: mostrar em interface gráfica lista de dispositivos para usuário selecionar e parear
-        val device = pairedDevices.find { it.address == "88:78:73:13:3A:C0" }
-
-        if (device == null) {
-            Log.d("seila", "Não encontrei o pc >-<")
-            return
-        }
-
-        thread {
-            adapter.cancelDiscovery()
-
-            // que horror
-            val socket = device.javaClass.getMethod("createRfcommSocket", *arrayOf(Int::class.java))
-                .invoke(device, 1) as BluetoothSocket
-
-            socket.use {
-                Log.d("seila", "consegui o socket")
-
-                socket.connect()
-
-                Log.d("seila", "conectado no socket")
-
-                this.bluetoothSocket = socket
-
-                onConnect()
-            }
-        }
-    }
+    val stuff: BluetoothStuff = BluetoothStuff()
 
     data class Message(
         val x: Double,
@@ -66,34 +16,14 @@ class MainActivity : AppCompatActivity() {
         val z: Double
     )
 
-    fun serializeMessage(message: Message, stream: OutputStream) {
-        val result = JSONObject()
-        result.put("x", message.x)
-        result.put("y", message.y)
-        result.put("z", message.z)
-
-        val bytes = result.toString().toByteArray(Charsets.UTF_8)
-
-        val newStream = DataOutputStream(stream)
-
-        newStream.writeInt(bytes.size)
-        newStream.write(bytes)
-        newStream.flush()
-    }
-
-    fun sendMessage(message: Message) {
-        val socket = bluetoothSocket!!
-
-        serializeMessage(message, socket.outputStream)
-    }
-
     fun onConnect() {
-        sendMessage(Message(1.0, 1.0, 1.0))
+        // código que será executado quando/se a conexão bluetooth for estabelecida
+        stuff.sendMessage(Message(1.0, 2.0, 3.0))
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        this.bluetoothSocket?.close()
+        this.stuff.close()
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -109,7 +39,9 @@ class MainActivity : AppCompatActivity() {
         val helper = BluetoothBoilerPlate(this)
 
         helper.callback = { adapter ->
-            connectBluetooth(adapter)
+            stuff.connectBluetooth(adapter) {
+                onConnect()
+            }
         }
 
         helper.setup()
